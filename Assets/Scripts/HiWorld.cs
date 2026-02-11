bb8f6fd7efd93cd5ba33f2494acceda735dc88d7
Dyle Ouano <geno683@gmail.com>
2026-02-11 18:47:24 +0800

    Modified and Added code
    -Modified HelloWorld script
    -Added HiWorld script

--- Assets/Scripts/HiWorld.cs
+++ Assets/Scripts/HiWorld.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class HiWorld : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Hello World!!!!"); 
+        Debug.Log("Hi World!");
     }
 
     // Update is called once per frame
